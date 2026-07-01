dbf60628ebc32ed171ebd32f091ec5007253ef8c
Fahreza <111201911809@mhs.dinus.ac.id>
2026-07-02 03:34:38 +0700

    commit 2 mengerjakan soal 2 conditional.php

---
+++ tugas-8/php-2/conditional.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Function</title>
+</head>
+
+<body>
+<h1>Berlatih Function PHP</h1>
+<?php
+
+echo "<h3> Soal No 1 Greetings </h3>";
+
+
+/* 
+Soal No 1
+Greetings
+Buatlah sebuah function greetings() yang menerima satu parameter berupa string. 
+
+contoh: greetings("abduh");
+Output: "Halo Abduh, Selamat Datang di Sanbercode!"
+*/
+
+// Code function di sini
+function greetings($name) {
+    echo "Halo $name, Selamat Datang di Sanbercode!<br>";
+}
+$name = "Bagas";
+greetings($name);
+$name = "Wahyu";
+greetings($name);
+$name = "nama peserta";
+greetings($name);
+
+
+
+
+// Hapus komentar untuk menjalankan code!
+// greetings("Bagas");
+// greetings("Wahyu");
+// greetings("nama peserta");
+
+echo "<br>";
+
+echo "<h3>Soal No 2 Reverse String</h3>";
+/* 
+Soal No 2
+Reverse String
+Buatlah sebuah function reverseString() untuk mengubah string berikut menjadi kebalikannya menggunakan function dan looping (for/while/do while).
+Function reverseString menerima satu parameter berupa string.
+NB: DILARANG menggunakan built-in function PHP sepert strrev(), HANYA gunakan LOOPING!
+
+reverseString("abdul");
+Output: ludba
+
+*/
+
+// Code function di sini 
+reverseString("abdul");
+function reverseString($str) {
+    $reversed = "";
+    for ($i = strlen($str) - 1; $i >= 0; $i--) {
+        $reversed .= $str[$i];
+    }
+    echo $reversed . "<br>";
+}
+
+// Hapus komentar di bawah ini untuk jalankan Code
+// reverseString("nama peserta");
+// reverseString("Sanbercode");
+// reverseString("We Are Sanbers Developers");
+echo "<br>";
+
+
+
+echo "<h3>Soal No 3 Tentukan Nilai </h3>";
+/*
+Soal 3
+buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter 
+berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” 
+Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar 
+sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang”
+*/
+
+// Code function di sini
+function tentukan_nilai($nilai) {
+    if ($nilai >= 85 && $nilai <= 100) {
+        return "Sangat Baik";
+    } elseif ($nilai >= 70 && $nilai < 85) {
+        return "Baik";
+    } elseif ($nilai >= 60 && $nilai < 70) {
+        return "Cukup";
+    } else {
+        return "Kurang";
+    }
+}
+$nilai = 98;
+echo tentukan_nilai($nilai) . "<br>";
+$nilai = 76;
+echo tentukan_nilai($nilai) . "<br>";
+$nilai = 67;
+echo tentukan_nilai($nilai) . "<br>";
+$nilai = 43;
+echo tentukan_nilai($nilai) . "<br>";
+// Hapus komentar di bawah ini untuk jalankan code
+// echo tentukan_nilai(98); //Sangat Baik
+// echo tentukan_nilai(76); //Baik
+// echo tentukan_nilai(67); //Cukup
+// echo tentukan_nilai(43); //Kurang
+
+
+?>
+
+</body>
+
+</html>
