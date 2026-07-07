791d65c569325955e9063e33975c483239046d18
Fahreza <111201911809@mhs.dinus.ac.id>
2026-07-08 03:01:03 +0700

    update router

--- tugas-12/inventoryapp_fahreza/routes/web.php
+++ tugas-12/inventoryapp_fahreza/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'home']);
+
+Route::get('/register', [FormController::class, 'resgister']);
+Route::post('/welcome', [FormController::class, 'welcome']);
