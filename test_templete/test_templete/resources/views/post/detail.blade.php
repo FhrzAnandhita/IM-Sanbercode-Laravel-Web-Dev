aed83abf7960f41ddd75cedfd5a5572ec84f91fe
Fahreza <111201911809@mhs.dinus.ac.id>
2026-07-09 04:28:56 +0700

    update

---
+++ test_templete/test_templete/resources/views/post/detail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Detail</title>
+</head>
+<body>
+    <h1>Detail Post</h1>
+    <p>Title: {{ $post->title }}</p>
+    <p>Content: {{ $post->content }}</p>
+</body>
+</html>
